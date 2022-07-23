a9174d6656f7e163ae14f22bfa657bb7282ab923
Joshua Prior <joshuaprior.github@gmail.com>
2022-07-23 06:35:29 -0700

    add mocha report container

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import css from 'mocha/mocha.css';
 
 const { describe, it } = mocha.constructor;
 
+const report = document.createElement('DIV');
+report.id = 'mocha';
+Object.apply(report.style, {
+  position: 'fixed',
+  zIndex: 100000,
+  backgroundColor: 'white',
+  margin: '0px',
+  inset: '0px 0px 0px 0px',
+});
+document.body.appendChild(report);
+
 console.log('Mocha -----------');
 console.log(Mocha);
 console.log('describe', describe);
