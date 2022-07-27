2f074e6f6a87ceb70c10caf9557f754edbc2f794
Joshua Prior <joshuaprior.github@gmail.com>
2022-07-27 08:41:01 -0700

    monkey patch mocha html reporter

--- src/index.js
+++ src/index.js
@@ -19,9 +19,36 @@ console.log(Mocha);
 console.log('describe', describe);
 console.log('it', it);
 console.log('Mocha -----------');
-debugger;
+
+class HTML2 extends Mocha.reporters.html {
+  constructor(runner, options) {
+    super(runner, options);
+    this.monkeyPatchCSS();
+    this.monkeyPatchHTML();
+  }
+
+  monkeyPatchCSS() {
+    const cssText = [...document.styleSheets]
+    .flatMap(sheet => [...sheet.cssRules])
+    .filter(({selectorText: sel}) => sel?.includes('#mocha-stats') && sel?.includes('.progress'))
+    .at(0)
+    .cssText
+    .replace('.progress', '.mocha-progress');
+
+    const style = document.createElement('STYLE');
+    style.textContent = cssText;
+    document.head.appendChild(style);
+  }
+
+  monkeyPatchHTML() {
+    const progress = document.querySelector('#mocha-stats .progress');
+    progress.classList.remove('progress');
+    progress.classList.add('mocha-progress');
+  }
+}
 
 mocha.ui('bdd');
+mocha.reporter(HTML2);
 describe('foobar', () => {
   it('foobaz', () => {
     console.log('a test');
